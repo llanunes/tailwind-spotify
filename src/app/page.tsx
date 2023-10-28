4f92655ae63feb03dc97d3c6f40daaec1a5f1158
Jaime alves <larissanunesvaz23@gmail.com>
2023-10-28 20:13:09 -0300

    spotify

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
-import Image from 'next/image'
 
-export default function Home() {
+
+ function Home() {
   return (
-    <div className='h-screen'>
-      <div>
-        <aside></aside>
-        <main></main>
+    <div className='h-screen flex flex-col'>
+      <div className='flex flex-1'>
+        <aside  className='w-72 bg-zinc-950 p-6'> 
+         </aside>
+        <main className='flex-1 p-6'> main </main>
       </div>
-      <footer></footer>
+      <footer className='bg-zinc-800 border-t border-zinc-700 p-6'> footer </footer>
     </div>
   )
 }
+
+export default Home
